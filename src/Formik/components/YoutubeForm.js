dfdb80c688666d54d41fc254ea0d048c775883a6
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-26 20:31:04 +0530

    useFormik hook

--- src/Formik/components/YoutubeForm.js
+++ src/Formik/components/YoutubeForm.js
@@ -1,26 +1,50 @@
-import { useFormik } from 'formik'
-import React from 'react'
-
+import { useFormik } from "formik";
+import React from "react";
 
 function YoutubeForm() {
-  const formik = useFormik({})
-  return (
-    <div>
-      <form>
-        <label htmlFor='name' >Name</label>
-        <input type="text" id='name' name='name' />
-        
-        <label htmlFor='email' >E-mail</label>
-        <input type="email" id='email' name='email' />
-        
-        <label htmlFor='channel' >Channel</label>
-        <input type="text" id='channel' name='channel' />
+	const formik = useFormik({
+		initialValues: {
+			name: "Chakshu",
+			email: "",
+			channel: "",
+		},
+  });
+  console.log('From values:',formik.values);
+	return (
+		<div>
+			<form>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					onChange={formik.handleChange}
+					value={formik.values.name}
+				/>
 
-        <button>Submit</button>
-      </form>
-    </div>
-  )
+				<label htmlFor="email">E-mail</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					onChange={formik.handleChange}
+					value={formik.values.email}
+				/>
+
+				<label htmlFor="channel">Channel</label>
+				<input
+					type="text"
+					id="channel"
+					name="channel"
+					onChange={formik.handleChange}
+					value={formik.values.channel}
+				/>
+
+				<button>Submit</button>
+			</form>
+		</div>
+	);
 }
 
-export default YoutubeForm
+export default YoutubeForm;
 // cmd for intsalling formik - npm install formik --save
